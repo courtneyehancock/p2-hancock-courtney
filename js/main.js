3098a77a85cc1ddf05864876050df664e67a4ac1
Courtney Hancock <ceh.courtney@gmail.com>
2019-07-06 13:13:12 -0700

    Edit mobile navigation

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var mlink = document.getElementById("morelink"),
+/*var mlink = document.getElementById("morelink"),
   mdiv = document.getElementById("morediv");
 
 mlink.addEventListener("click", function(event) {
@@ -6,4 +6,37 @@ mlink.addEventListener("click", function(event) {
   mdiv.style.display = (mdiv.style.display === "none") ? "block" : "none";
 }, false);
 
-mdiv.style.display = "none";
+mdiv.style.display = "none";*/
+
+var openButton = document.getElementById('menu-open');
+var closeButton = document.getElementById('menu-close');
+var mobileMenu = document.getElementById('mobile-nav');
+
+//we need our mobile menu to be hidden
+
+//showMenu function
+function showMenu() {
+  closeButton.classList.remove('hide');
+  mobileMenu.classList.remove('hide');
+  openButton.classList.add('hide');
+  closeButton.classList.add('show');
+  mobileMenu.classList.add('show');
+  openButton.classList.remove('show');
+};
+
+//hideMenu function
+function hideMenu(){
+  closeButton.classList.add('hide');
+  mobileMenu.classList.add('hide');
+  openButton.classList.remove('hide');
+  closeButton.classList.remove('show');
+  mobileMenu.classList.remove('show');
+  openButton.classList.add('show');
+
+};
+
+//add click event listener to our menuOpen button
+openButton.addEventListener('click', showMenu);
+
+//add click event listener to our menuClose button
+closeButton.addEventListener('click', hideMenu);
