fcee9a526bdfc503a8cdf4e1401d75491390f7d8
Courtney Hancock <ceh.courtney@gmail.com>
2019-07-08 21:13:05 -0700

    Edit javascript for read more

--- js/main.js
+++ js/main.js
@@ -1,42 +1,13 @@
-/*var mlink = document.getElementById("morelink"),
-  mdiv = document.getElementById("morediv");
+var button = document.getElementById('show-message');
+var hiddenMessage = document.getElementById('hidden-message');
 
-mlink.addEventListener("click", function(event) {
-  event.preventDefault();
-  mdiv.style.display = (mdiv.style.display === "none") ? "block" : "none";
-}, false);
+function showMessage() {
+  hiddenMessage.classList.remove('hide');
+}
 
-mdiv.style.display = "none";*/
+function hideMessage() {
+  hiddenMessage.classList.add('hide');
+}
 
-var openButton = document.getElementById('menu-open');
-var closeButton = document.getElementById('menu-close');
-var mobileMenu = document.getElementById('mobile-nav');
-
-//we need our mobile menu to be hidden
-
-//showMenu function
-function showMenu() {
-  closeButton.classList.remove('hide');
-  mobileMenu.classList.remove('hide');
-  openButton.classList.add('hide');
-  closeButton.classList.add('show');
-  mobileMenu.classList.add('show');
-  openButton.classList.remove('show');
-};
-
-//hideMenu function
-function hideMenu(){
-  closeButton.classList.add('hide');
-  mobileMenu.classList.add('hide');
-  openButton.classList.remove('hide');
-  closeButton.classList.remove('show');
-  mobileMenu.classList.remove('show');
-  openButton.classList.add('show');
-
-};
-
-//add click event listener to our menuOpen button
-openButton.addEventListener('click', showMenu);
-
-//add click event listener to our menuClose button
-closeButton.addEventListener('click', hideMenu);
+button.onmouseenter = showMessage;
+button.onmouseleave = hideMessage;
